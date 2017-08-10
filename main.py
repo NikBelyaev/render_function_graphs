01f8acba608e4aba956324328ef6d121d8f4ef02
1mace1 <belyaev.nikit@gmail.com>
2017-08-11 00:34:11 +0300

    Added function "format_input_func"

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import re
+
+def format_input_func(formula):
+    # Search for | symbols and replace it with abs() method
+    re_result_list = re.findall('(\|.*?\|)', formula)
+    for re_result in re_result_list:
+        formula = formula.replace(re_result, 'abs('+re_result.strip('|')+')')
+    return formula
 
 def render_func_graph(formula, x_range):
     try:
         x = np.array(x_range)
         y = eval(formula)
-        ## Instructions for func graph rendering
-        fig = plt.figure('Function graph')        
+        ## Set the settings to display graph
+        fig = plt.figure() 
+        plt.xticks(np.arange(min(x), max(x)+1, 1.0))
         ax = fig.add_subplot(111)
+        ## Set the Axis OX and OY crossed on zero
         ax.spines['left'].set_position('zero')
         ax.spines['right'].set_color('none')
         ax.spines['bottom'].set_position('zero')
@@ -16,16 +26,19 @@ def render_func_graph(formula, x_range):
         ax.spines['bottom'].set_smart_bounds(True)
         ax.xaxis.set_ticks_position('bottom')
         ax.yaxis.set_ticks_position('left')
+        plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+        plt.plot(x, y, 'go-')
         plt.scatter(0,0)
         plt.grid(True)
-        plt.plot(x, y, 'go-')
         plt.show()
     except Exception as err:
-        print(str(err))
+        print(str(err))  
 
 def main():
     func = input('f(x):')
-    render_func_graph(func, np.arange(-5, 5, 0.5))
+    formula = format_input_func(func)
+    print(formula)
+    render_func_graph(formula, np.arange(-5, 5, 0.5))
 
 if __name__ == '__main__':
     main()
