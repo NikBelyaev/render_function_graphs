eb791a9416a33083de272350e61274a5d99bfd1a
1mace1 <belyaev.nikit@gmail.com>
2017-08-10 23:35:22 +0300

    Added OX and OY Axis

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import numpy as np
 
 def render_func_graph(formula, x_range):
     try:
-        fig = plt.figure()    
         x = np.array(x_range)
         y = eval(formula)
-        print(y)
-        plt.plot(x, y, 'go-')
+        ## Instructions for func graph rendering
+        fig = plt.figure('Function graph')        
+        ax = fig.add_subplot(111)
+        ax.spines['left'].set_position('zero')
+        ax.spines['right'].set_color('none')
+        ax.spines['bottom'].set_position('zero')
+        ax.spines['top'].set_color('none')
+        ax.spines['left'].set_smart_bounds(True)
+        ax.spines['bottom'].set_smart_bounds(True)
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
         plt.scatter(0,0)
         plt.grid(True)
+        plt.plot(x, y, 'go-')
         plt.show()
     except Exception as err:
-        print(str(err))  
+        print(str(err))
 
 def main():
     func = input('f(x):')
-    render_func_graph(func, range(-10, 10))
+    render_func_graph(func, np.arange(-5, 5, 0.5))
 
 if __name__ == '__main__':
     main()
