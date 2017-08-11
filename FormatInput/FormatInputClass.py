e3e860125254ac173b93c90c487f14bc78291ee3
1mace1 <belyaev.nikit@gmail.com>
2017-08-11 13:48:39 +0300

    Code improve by pep8 standart

--- FormatInput/FormatInputClass.py
+++ FormatInput/FormatInputClass.py
@@ -2,20 +2,19 @@ import re
 import numpy as np
 from config import shape_of_x_range, x_step
 
+
 class FormatInputClass:
     def __init__(self, formula, x_range):
         self.formula = formula
         self.x_range = x_range
-
 
     def format_vertical_slashes(self):
         """Replaces vertical slashes (|) with abs() method"""
         re_result_list = re.findall('(\|.*?\|)', self.formula)
         for re_result in re_result_list:
             self.formula = self.formula.replace(re_result,
-                                            'abs('+re_result.strip('|')+')')
+                                                'abs(' + re_result.strip('|') + ')')
         return self.formula
-
 
     def format_trigon_funcs(self):
         """Adds prefix (np.) before all trigon. functions"""
@@ -24,13 +23,12 @@ class FormatInputClass:
         for trigon_func in trigon_funcs:
             if trigon_func in self.formula:
                 self.formula = self.formula.replace(trigon_func,
-                                                    'np.'+trigon_func)
+                                                    'np.' + trigon_func)
         return self.formula
-
 
     def format_system_of_equations(self):
         """Splits system of equations to simple equations"""
-        comp_symbols = ['>[^=]', '<[^=]', '>=', '<=','[^<>]=']
+        comp_symbols = ['>[^=]', '<[^=]', '>=', '<=', '[^<>]=']
         list_of_dicttype_formulas = []
         for n_formula in self.formula[1:].replace(' ', '').split(';'):
             new_formula, new_comp = n_formula.split(',')
@@ -40,24 +38,23 @@ class FormatInputClass:
                     re_result = re.sub('\d', '', re_result.group(0))
                     x_value = int(new_comp.split(re_result)[1])
                     if comp_symbol == '>[^=]':
-                        self.x_range = np.arange(x_value+1,
-                                                 x_value+shape_of_x_range+1,
+                        self.x_range = np.arange(x_value + 1,
+                                                 x_value + shape_of_x_range + 1,
                                                  x_step)
                     elif comp_symbol == '<[^=]':
-                        self.x_range = np.arange(x_value-shape_of_x_range+1,
-                                                 x_value-1, x_step)
+                        self.x_range = np.arange(x_value - shape_of_x_range + 1,
+                                                 x_value - 1, x_step)
                     elif comp_symbol == '>=':
                         self.x_range = np.arange(x_value,
-                                                 x_value+shape_of_x_range,
+                                                 x_value + shape_of_x_range,
                                                  x_step)
                     elif comp_symbol == '<=':
-                        self.x_range = np.arange(x_value-shape_of_x_range,
+                        self.x_range = np.arange(x_value - shape_of_x_range,
                                                  x_value, x_step)
                     list_of_dicttype_formulas.append(dict(
-                        {'formula':new_formula, 'x_range':self.x_range}))
+                        {'formula': new_formula, 'x_range': self.x_range}))
         return list_of_dicttype_formulas
 
-    
     def wrapper_format_input(self):
         """Wraps all the formating input functions"""
         try:
@@ -66,6 +63,6 @@ class FormatInputClass:
             else:
                 self.formula = self.format_vertical_slashes()
                 self.formula = self.format_trigon_funcs()
-                return [{'formula':self.formula, 'x_range':self.x_range}]
+                return [{'formula': self.formula, 'x_range': self.x_range}]
         except Exception as error:
             print(str(error))
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import numpy as np
 from FormatInput.FormatInputClass import FormatInputClass
 from config import shape_of_x_range, x_step
 
+
 def render_func_graph(formula_range_list_dict):
     try:
-        ## Set the settings to display graph
-        fig = plt.figure('Function graph') 
+        # Set the settings to display graph
+        fig = plt.figure('Function graph')
         ax = fig.add_subplot(111)
-        ## Set the Axis OX and OY crossed on zero
+        # Set the Axis OX and OY crossed on zero
         ax.spines['left'].set_position('zero')
         ax.spines['right'].set_color('none')
         ax.spines['bottom'].set_position('zero')
@@ -17,22 +18,23 @@ def render_func_graph(formula_range_list_dict):
         ax.spines['bottom'].set_smart_bounds(True)
         ax.xaxis.set_ticks_position('bottom')
         ax.yaxis.set_ticks_position('left')
-        plt.scatter(0,0)
+        plt.scatter(0, 0)
         plt.grid(True)
-        
-        for formula_range_dict in formula_range_list_dict:  
+
+        for formula_range_dict in formula_range_list_dict:
             x = np.array(formula_range_dict.get('x_range'))
             y = eval(str(formula_range_dict.get('formula')))
             plt.plot(x, y, 'go-')
         plt.show()
     except Exception as err:
-        print(str(err))  
+        print(str(err))
+
 
 def main():
     print('Type "q" to exit the program.\n\n')
     while True:
         func = input('f(x):')
-        if func=='q':
+        if func == 'q':
             break
         format_input = FormatInputClass(func, np.arange(-shape_of_x_range,
                                                         shape_of_x_range,
@@ -40,6 +42,6 @@ def main():
         formula_range_list_dict = format_input.wrapper_format_input()
         render_func_graph(formula_range_list_dict)
 
+
 if __name__ == '__main__':
     main()
-
