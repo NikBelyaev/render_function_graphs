469bdea70ede021ec4f0e6d6e4e0f7d8d59d531a
1mace1 <belyaev.nikit@gmail.com>
2017-08-16 13:25:34 +0300

    Added support for system of equations
    There're some bugs with it. One of them is unable to calculate system of
    eq twice

--- RenderFunctionGrapghs/Obj/FormatInputClass.py
+++ RenderFunctionGrapghs/Obj/FormatInputClass.py
@@ -1,13 +1,14 @@
 import re
 import numpy as np
-
+from Obj.config import *
 
 class FormatInput:
     def __init__(self, formula):
         self.formula = formula
 
 
-    def replace_change_math_symbols(self):
+    def basic_format(self):
+        self.formula = self.formula.replace(' ', '')
         self.formula = self.formula.replace('÷', '/')
         self.formula = self.formula.replace('^', '**')
          
@@ -32,6 +33,35 @@ class FormatInput:
                 # Adding *
                 tmp = res.split(res[len(res)-1:])[0]+'*'+res[len(res)-1:]
                 self.formula = self.formula.replace(res, tmp)
+
+
+    def format_comma_statement(self):
+        comp_symbols = ['>[^=]', '<[^=]', '>=', '<=', '[^<>]=']
+        
+        self.formula, new_comp = self.formula.split(',')
+
+        for comp_symbol in comp_symbols:
+            re_result = re.search(comp_symbol, new_comp)
+            if re_result is not None:
+                re_result = re.sub('\d', '', re_result.group(0))
+                x_value = int(new_comp.split(re_result)[1])
+                if comp_symbol == '>[^=]':
+                    self.x_range = np.arange(x_value + 1,
+                                             x_value + shape_of_x_range + 1,
+                                             x_step)
+                elif comp_symbol == '<[^=]':
+                    self.x_range = np.arange(x_value - shape_of_x_range + 1,
+                                             x_value - 1, x_step)
+                elif comp_symbol == '>=':
+                    self.x_range = np.arange(x_value,
+                                             x_value + shape_of_x_range,
+                                             x_step)
+                elif comp_symbol == '<=':
+                    self.x_range = np.arange(x_value - shape_of_x_range,
+                                             x_value, x_step)
+            else:
+                self.x_range = None
+        
 ##
 ##    def format_system_of_equations(self):
 ##        """Splits system of equations to simple equations"""
@@ -62,13 +92,17 @@ class FormatInput:
 ##                        {'formula': new_formula, 'x_range': self.x_range}))
 ##        return list_of_dicttype_formulas
 
-    def wrapper_format_input(self):
+    def wrapper_format_input(self, system_of_eq=False):
         """Wraps all the formating input functions"""
         try:
-            self.replace_change_math_symbols()
+            self.basic_format()
             self.format_vertical_slashes()
             self.format_sqrt_symbol()
             self.format_multiply()
-            return self.formula
+            if system_of_eq:
+                self.format_comma_statement()
+                return self.formula, self.x_range
+            else:   
+                return self.formula
         except Exception as error:
             print('[!!] Error in FormatInput', str(error))
--- RenderFunctionGrapghs/Obj/InputWindowClass.py
+++ RenderFunctionGrapghs/Obj/InputWindowClass.py
@@ -11,73 +11,117 @@ class InputWindow(wx.Dialog):
     '''Main calculator dialog'''
     def __init__(self):
         wx.Dialog.__init__(self, None, -1, 'Render Graph')    
-        sizer = wx.BoxSizer(wx.VERTICAL) # Main vertical sizer
+        self.mainSizer = wx.BoxSizer(wx.VERTICAL) # Main vertical sizer
 
+        self.system_of_eq = False
+        
         # ____________v
+        self.inputBoxId = 0
+        self.displayList = []
+        self.inputSizer = wx.BoxSizer(wx.VERTICAL)
         self.display = wx.ComboBox(self, -1) # Current calculation
-        sizer.Add(self.display, 0, wx.EXPAND) # Add to main sizer
-
+##        self.display.Bind(wx.EVT_KILL_FOCUS, self.OnFocus)
+        self.inputSizer.Add(self.display, 0, wx.EXPAND) # Add to main sizer
+        self.displayList.append(self.display)
+        
+        self.mainSizer.Add(self.inputSizer, 0, wx.EXPAND)
         # [7][8][9][/] 
         # [4][5][6][*]
         # [1][2][3][-]
         # [0][.][C][+]
         gsizer = wx.GridSizer(4, 5, 1)
+        self.buttonSizer = wx.BoxSizer(wx.VERTICAL)
         for row in (('sin()', 'cos()', 'tan()', 'x'),
                     ('|', '(', ')', '^'),
                     ('7', '8', '9', '÷'),
                     ('4', '5', '6', '*'),
                     ('1', '2', '3', '-'),
-                    ('0', '√()','C', '+')):
+                    ('0', '√()','{', '+'),
+                    'C'):
             for label in row:
                 b = wx.Button(self, -1, label)
                 gsizer.Add(b)
                 self.Bind(wx.EVT_BUTTON, self.OnButton, b)
-        sizer.Add(gsizer, 1, wx.EXPAND)
+        self.buttonSizer.Add(gsizer, 1, wx.EXPAND)
 
         # [    =     ]
         b = wx.Button(self, -1, '=')
         self.Bind(wx.EVT_BUTTON, self.OnButton, b)
-        sizer.Add(b, 0, wx.EXPAND)
+        self.buttonSizer.Add(b, 0, wx.EXPAND)
         self.equal = b
 
+        self.mainSizer.Add(self.buttonSizer, 0, wx.EXPAND)
+        
         # Set sizer and center
-        self.SetSizer(sizer)
-        sizer.Fit(self)
+        self.SetSizer(self.mainSizer)
+        self.mainSizer.Fit(self)
         self.CenterOnScreen()
+
+
+##    def OnFocus(self, evt):
+##        self.focused = evt.GetEventObject()
 
         
     def OnButton(self, evt):
         '''Handle button click event'''
         # Get title of clicked button
         label = evt.GetEventObject().GetLabel() 
-
+        if label == '{':
+            
+            if not self.inputBoxId == 2:
+                self.system_of_eq = True
+                self.inputBoxId+=1
+                try:
+                    del(self.newDisplay)
+                except:
+                    pass
+                self.newDisplay = wx.ComboBox(self, -1)
+    ##            self.display2.Bind(wx.EVT_KILL_FOCUS, self.OnFocus)
+                self.inputSizer.Add(self.newDisplay, 0, wx.EXPAND)
+                self.mainSizer.Layout()
+                self.displayList.append(self.newDisplay)
+                self.display.SetValue('')
         if label == '=': # Calculate
             try:
-                compute = self.display.GetValue()
-                # Ignore empty calculation
-                if not compute.strip():
-                    return
-
-                # Add to history
-                self.display.Insert(compute, 0)
-                
                 formula_range_list_dict = []
-                self.format_input = FormatInput(compute)
-                self.finished_formula = self.format_input.wrapper_format_input()
-                formula_range_list_dict.append(dict({'formula': self.finished_formula,
-                                                     'x_range': np.arange(-cfg.shape_of_x_range,
-                                                                cfg.shape_of_x_range,
-                                                                cfg.x_step)}))
+                for inp in self.displayList:
+                    compute = inp.GetValue()
+                    # Ignore empty calculation
+                    if not compute.strip():
+                        return
 
-                self.display.SetValue('')
+                    # Add to history
+                    inp.Insert(compute, 0)
+                    
+                    self.format_input = FormatInput(compute)
+                    finished_formula, x_range = self.format_input.wrapper_format_input(system_of_eq = self.system_of_eq)
+                    if x_range:
+                        formula_range_list_dict.append(dict({'formula': finished_formula,
+                                                             'x_range': x_range}))
+                    else:
+                        formula_range_list_dict.append(dict({'formula': finished_formula,
+                                                             'x_range': np.arange(-cfg.shape_of_x_range,
+                                                                        cfg.shape_of_x_range,
+                                                                        cfg.x_step)}))
+
+                    inp.SetValue('')
                 render_func_graph(formula_range_list_dict)
+                if len(self.displayList)==2:
+                    self.inputSizer.Remove(1)
+                    self.mainSizer.Layout()
+                    self.displayList.pop(1)
+                self.system_of_eq = False
             except Exception as e:
                 wx.LogError(str(e))
                 return
 
         elif label == 'C': # Clear
-            self.display.SetValue('')
+            for inp in self.displayList:
+                inp.SetValue('')
 
         else: # Just add button text to current calculation
+##            self.focused.SetValue(self.focused.GetValue() + label)
             self.display.SetValue(self.display.GetValue() + label)
-            self.equal.SetFocus() # Set the [=] button in focus
+
+##            self.equal.SetFocus() # Set the [=] button in focus
+##
